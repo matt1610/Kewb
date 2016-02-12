f76918ad35a2263071a1ec071abac6730d9c92cd
Matthew Starkey <mattstarkey@me.com>
2016-02-12 11:36:38 +0200

    Portal

---
+++ Assets/Portal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Boo.Lang;
+
+public class Portal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            
+        }
+    }
+
+}
